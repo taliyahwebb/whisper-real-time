2f4ac8efcb9fd85acc186eec523c2b4f297e99e0
Taliyah Webb <taliyahmail@proton.me>
2025-02-15 12:08:29 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "Flakebox Project template";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flakebox.url = "github:rustshop/flakebox";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      flakebox,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        projectName = "flakebox-project";
+
+        flakeboxLib = flakebox.lib.${system} {
+          config = {
+            github.ci.enable = false;
+            semgrep.enable = false;
+          };
+        };
+
+        buildPaths = [
+          "Cargo.toml"
+          "Cargo.lock"
+          "src"
+        ];
+
+        buildSrc = flakeboxLib.filterSubPaths {
+          root = builtins.path {
+            name = projectName;
+            path = ./.;
+          };
+          paths = buildPaths;
+        };
+
+        multiBuild = (flakeboxLib.craneMultiBuild { }) (
+          craneLib':
+          let
+            craneLib = (
+              craneLib'.overrideArgs {
+                pname = projectName;
+                src = buildSrc;
+                nativeBuildInputs = [ ];
+              }
+            );
+          in
+          {
+            ${projectName} = craneLib.buildPackage { };
+          }
+        );
+      in
+      {
+        packages.default = multiBuild.${projectName};
+
+        legacyPackages = multiBuild;
+
+        devShells = flakeboxLib.mkShells { };
+      }
+    );
+}
